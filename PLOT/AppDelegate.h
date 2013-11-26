10faece9607dc202c009067b389c42853d9fd4b0
Pascal Samoilov <nubaslon@gmail.com>
2013-11-26 17:08:22 +0400

    Initial Commit

---
+++ PLOT/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PLOT
+//
+//  Created by SAMO Soft on 26.11.13.
+//  Copyright (c) 2013 Nubaslon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PLOT/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PLOT
+//
+//  Created by SAMO Soft on 26.11.13.
+//  Copyright (c) 2013 Nubaslon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
